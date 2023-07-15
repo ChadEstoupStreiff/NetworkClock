107cd0e9c8beec0a619b24a06ce8f71f64d8f432
ChadOW <chad.estoup@gmail.com>
2023-07-15 15:23:26 +0200

    Admin app

--- src/main_admin.c
+++ src/main_admin.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
+#include <unistd.h>
 
-// IN DEV
-int main() {
+int main(int argc, char **argv)
+{
+    if (argc == 0)
+    {
+        perror("No argument given");
+        exit(1);
+    }
+    if (setuid(0))
+    {
+        perror("Permission denied");
+        exit(1);
+    }
+
     struct timeval tv;
 
-    tv.tv_sec = 1626349200;
+    tv.tv_sec = atoi(argv[1]);
     tv.tv_usec = 0;
 
     if (settimeofday(&tv, NULL) == -1)
-        return 1;
+    {
+        perror("Can't set time");
+        exit(1);
+    }
 
     return 0;
 }
